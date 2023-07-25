50d03d3255fd5050d1b22c433d4b11f0c4747765
Raman Kumar <138963337+RamanKumar001@users.noreply.github.com>
2023-07-25 05:31:47 +0530

    Add files via upload

---
+++ resume maker/main.js
@@ -0,0 +1,71 @@
+
+const inputfields = document.querySelector('.input-fields')
+const output = document.querySelector('.output')
+const btn = document.querySelector('.btn')
+const hea = document.querySelector('h1')
+const left = document.querySelector('.left')
+// const ima = document.querySelector('#image')
+let inputShow = true
+
+
+async function TextEditor(element) {
+    const newEditor = await ClassicEditor
+        .create(element, {
+            toolbar: ['heading', 'bold', 'italic', 'bulletedList', 'numberedList', 'blockQuote'],
+        })
+    return newEditor;
+
+}
+
+let workExpdetails;
+TextEditor(inputfields["workexp"]).then(nEditor => {
+    workExpdetails = nEditor
+})
+let Academic;
+TextEditor(inputfields["academics"]).then(nEditor => {
+    Academic = nEditor
+});
+
+function toggle() {
+    if (inputShow) {
+        inputfields.style.display = "none"
+        btn.style.display = "none"
+        // left.style.display = "none"
+        // left.style.marginTop = '50%';
+        hea.style.display = "none"
+        inputShow = false
+        output.innerHTML = `
+           <div class="main">
+               <div id="one">
+               <h1>${inputfields["name"].value}</h1>
+               <h3>${inputfields["title"].value}</h3><br><br>
+                 <h2>OBJECTIVE</h2>
+                 <p>${inputfields["objective"].value}</p>
+                 <h2>SKILLS</h2>
+                 <p>${inputfields["skills"].value}</p>
+                 <h2>ACHIEVEMENTS</h2>
+                 <p>${inputfields["achievements"].value}</p>
+                 <h2>WORK EXPERIENCE</h2>
+                ${workExpdetails.getData()}
+               </div>
+               <div>
+                <div.id="two">
+                <h2>ACEMEDIC DETAILS</h2>
+                 ${Academic.getData()}
+                 <h2>PROJECTS</h2>
+                <p>${inputfields["projects"].value}</p>
+                 <h2>CONTACT</h2>
+                 <p>${inputfields["contact"].value}</p>
+               </div>
+               </div>
+           </div>
+           <div class="btn">
+              <button onclick="print()">Print Resume</button>
+        </div>
+         `
+    } else {
+        inputfields.style.display = "block"
+        inputShow = true
+        output.innerHTML = ""
+    }
+}
